6b37e1a98fdedd214f9f6c0c0520be8d6bc625bb
kingsisland <kvsqashit@gmail.com>
2020-04-21 19:48:42 +0530

    Implemented sudoku solver that uses recursion, performs great.

--- Sudoku Solver/number_classifier.py
+++ Sudoku Solver/number_classifier.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 import cv2
@@ -17,7 +18,7 @@ class NumberClassifier:
                     continue
                 current_cell_path = "Output_Images/cropped_scaled_cells/" + str(row) + str(col) + ".png"
                 current_cell = cv2.imread(current_cell_path, 0)
-                print("The cell is %s" % ((row, col),))
+                # print("The cell is %s" % ((row, col),))
                 max_count = -1
                 classified_number = None
                 # Compare this current cell to all the existing training images
@@ -53,10 +54,91 @@ class NumberClassifier:
                             if matches > max_count:
                                 max_count = matches
                                 classified_number = number
-                                print("Current best match is %s with %d pixels matched" % ((number, variant), matches))
+                                # print("Current best match is %s with %d pixels matched" % ((number, variant), matches))
 
-                print("Finally, the classified number is %d with %d matches" % (classified_number, max_count))
+                # print("Finally, the classified number is %d with %d matches" % (classified_number, max_count))
                 self.sudoku_table[row, col] = classified_number
+
+
+class SudokuSolver:
+    def __init__(self, number_classifier=NumberClassifier()):
+        self.sudoku_table = number_classifier.sudoku_table
+        self.row_sets = list(set() for _ in range(9))
+        self.col_sets = list(set() for _ in range(9))
+        self.nonet = dict()
+        for i in range(3):
+            for j in range(3):
+                nonet_name = str(i) + str(j)
+                self.nonet[nonet_name] = set()
+
+        #  Filling out the row sets, col sets and the nonet sets. Preprocessing the initial numbers present on the board
+        for i in range(9):
+            for j in range(9):
+                if self.sudoku_table[i, j] == -1:
+                    continue
+                self.row_sets[i].add(self.sudoku_table[i, j])
+                self.col_sets[j].add(self.sudoku_table[i, j])
+                nonet_name = str(math.floor(i / 3)) + str(math.floor(j / 3))
+                self.nonet[nonet_name].add(self.sudoku_table[i, j])
+
+    def solve(self):
+        is_solved = None
+        flag = False
+        for i in range(9):
+            if flag:
+                break
+            for j in range(9):
+                if self.sudoku_table[i, j] != -1:
+                    continue
+
+                # If we come across an empty slot
+                is_solved = self.try_numbers_recursion(i, j)
+                flag = True
+                break
+        if is_solved:
+            print(self.sudoku_table)
+        else:
+            print("The sudoku puzzle could not be solved, sorry!")
+
+    def try_numbers_recursion(self, i, j):
+        # print("recursion %s" %((i,j ),))
+        nonet_name = str(math.floor(i / 3)) + str(math.floor(j / 3))
+        for num in range(1, 10):
+
+            if self.row_sets[i].__contains__(num) or self.col_sets[j].__contains__(num) or \
+                    self.nonet[nonet_name].__contains__(num):
+                continue
+            # We proceed to add the number to all the row, col and nonet sets and the sudoku table
+            self.row_sets[i].add(num)
+            self.col_sets[j].add(num)
+            self.nonet[nonet_name].add(num)
+            self.sudoku_table[i, j] = num
+            if (i, j) == (8, 8):  # Dont allow the function to call for out of bound indexes
+                return True
+
+            # Then we call the next element
+            row, col = i, j
+            flag = False
+            while row < 9:
+                while col < 9:
+                    if self.sudoku_table[row, col] == -1:
+                        flag = True
+                        break
+                    col += 1
+                if flag:
+                    break
+                row += 1
+                col = 0  # reset the col
+
+            if not self.try_numbers_recursion(row, col):
+                self.row_sets[i].remove(num)
+                self.col_sets[j].remove(num)
+                self.nonet[nonet_name].remove(num)
+                self.sudoku_table[i, j] = -1
+            else:
+                return True
+
+        return False
 
 
 number_extractor = NumberExtractor()
@@ -64,6 +146,7 @@ number_extractor.preprocess()
 number_extractor.find_numbers()
 number_classifier = NumberClassifier(number_extractor)
 number_classifier.classify_numbers()
-
+sudoku_solver = SudokuSolver(number_classifier)
+sudoku_solver.solve()
 # number_extractor.center_scale_numbers()
 # number_extractor.crop_scale_numbers()
