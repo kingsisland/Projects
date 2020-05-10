379ef97e0a59dc039c5dfe5e3994caec53161f81
kingsisland <kvsqashit@gmail.com>
2020-05-11 02:15:03 +0530

    added   gameamanger that manages the game states

--- Chess/Assets/Scripts/GameManager.cs
+++ Chess/Assets/Scripts/GameManager.cs
@@ -1,18 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    public Board board;
+
+    public GameObject whiteKing;
+    public GameObject whiteQueen;
+    public GameObject whiteBishop;
+    public GameObject whiteKnight;
+    public GameObject whiteRook;
+    public GameObject whitePawn;
+
+    public GameObject blackKing;
+    public GameObject blackQueen;
+    public GameObject blackBishop;
+    public GameObject blackKnight;
+    public GameObject blackRook;
+    public GameObject blackPawn;
+
+    private GameObject[,] pieces;
+
+    private Player white;
+    private Player black;
+    public Player currentPlayer;
+    public Player otherPlayer;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        pieces = new GameObject[8, 8];
+
+        white = new Player("white", true);
+        black = new Player("black", false);
+
+        currentPlayer = white;
+        otherPlayer = black;
+
+        InitialSetup();
     }
+
+    private void InitialSetup()
+    {
+        AddPiece(whiteRook, white, 0, 0);
+        AddPiece(whiteKnight, white, 1, 0);
+        AddPiece(whiteBishop, white, 2, 0);
+        AddPiece(whiteQueen, white, 3, 0);
+        AddPiece(whiteKing, white, 4, 0);
+        AddPiece(whiteBishop, white, 5, 0);
+        AddPiece(whiteKnight, white, 6, 0);
+        AddPiece(whiteRook, white, 7, 0);
+
+        for(int col = 0; col < 8; col++)
+        {
+            AddPiece(whitePawn, white, col, 1);
+        }
+
+        AddPiece(blackRook, black, 0, 7);
+        AddPiece(blackKnight, black, 1, 7);
+        AddPiece(blackBishop, black, 2, 7);
+        AddPiece(blackQueen, black, 3, 7);
+        AddPiece(blackKing, black, 4, 7);
+        AddPiece(blackBishop, black, 5, 7);
+        AddPiece(blackKnight, black, 6, 7);
+        AddPiece(blackRook, black, 7, 7);
+
+        for (int col = 0; col < 8; col++)
+        {
+            AddPiece(blackPawn, black, col, 6);
+        }       
+    }
+
+    public void AddPiece(GameObject prefab, Player player, int col, int row)
+    {
+        GameObject pieceObject = board.AddPiece(prefab, col, row);
+        player.pieces.Add(pieceObject);
+        pieces[col, row] = pieceObject;
+    }
+
+   
 }
